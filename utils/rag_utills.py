a6a3a8d349790b6b30da716f6f9ba6fc52fe6e7a
Jay Shirgupe <jns29072003@gmail.com>
2024-10-09 21:35:56 +0530

    Added metadat field to file upload

--- utils/rag_utills.py
+++ utils/rag_utills.py
@@ -1,11 +1,16 @@
 from typing import IO
 from db_utils import store_text
 
-def upload_file(file: IO) -> None:
+def upload_file(file: IO, name: str, description: str, department: str) -> None:
     print(file.name)
     text = file.read()
     text = text[:1200]
-    store_text(text, {"department": "finance"})
+    metadata = {
+        "name": name,
+        "department": department,
+        "description": description,
+    }
+    store_text(text, metadata)
 
 def answer_query(query: str) -> str:
     return "Hello! This is Enterprise-RAG!"
@@ -13,4 +18,4 @@ def answer_query(query: str) -> str:
 
 if __name__ == "__main__":
     with open("book.txt", "r") as file:
-        upload_file(file)
+        upload_file(file, "book", "finance", "just a finance book")
