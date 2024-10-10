753f7b488c438b36856102dcf14ea4173238d078
Jay Shirgupe <jns29072003@gmail.com>
2024-10-10 23:15:04 +0530

    Added upload text and stub for image and table upload

--- utils/rag_utills.py
+++ utils/rag_utills.py
@@ -1,11 +1,9 @@
 from typing import IO
 from db_utils import store_text
 from chains import rag_chain
+import pandas as pd
 
-def upload_file(file: IO, name: str, description: str, department: str) -> None:
-    print(file.name)
-    text = file.read()
-    text = text[:1200]
+def upload_text(text: str, name: str, description: str, department: str) -> None:
     metadata = {
         "name": name,
         "department": department,
@@ -13,12 +11,21 @@ def upload_file(file: IO, name: str, description: str, department: str) -> None:
     }
     store_text(text, metadata)
 
+def upload_table(df: pd.DataFrame, name: str, description: str, department: str) -> None:
+    pass
+
+def upload_image(img, name: str, description: str, department: str) -> None:
+    pass
+
 def answer_query(query: str) -> str:
     return rag_chain.invoke(query)
 
 
 if __name__ == "__main__":
 #    with open("book.txt", "r") as file:
-#        upload_file(file, "book", "finance", "just a finance book")
-    print(answer_query("What is the book about ?"))
-    print(answer_query("Who has published this book ?"))
+#        text = file.read()
+#        text = text[:1000]
+#        upload_text(text, "book", "finance", "just a finance book")
+#    print(answer_query("What is the book about ?"))
+#    print(answer_query("Who has published this book ?"))
+    pass
