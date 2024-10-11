edf5de870a35fef4cdaa7f185a4662fbf9c4185e
Jay Shirgupe <jns29072003@gmail.com>
2024-10-11 23:03:48 +0530

    Integrated off-shelf rag chain

--- server/chains/qa.py
+++ server/chains/qa.py
@@ -1,22 +1,24 @@
 from .query_classification import query_classification_chain
 from .table_query import csv_agent
-from .simple_rag import simple_rag_chain
+from .rag_chain import rag_chain
 from langchain_core.runnables import RunnablePassthrough
 from langchain_core.runnables import RunnableLambda
 
 def route(info):
     if info['query_type'] == 'Data Retrieval':
-        return simple_rag_chain
+        return rag_chain
     elif info['query_type'] == 'Data Computation':
         return csv_agent
 
 def parse_output(info):
-    if type(info) == str:
-        return info
-    return info['output']
+    if 'answer' in info.keys():
+        return info['answer']
+    if 'output' in info.keys():
+        return info['output']
+    return "Query Failed"
 
 qa_chain = (
-    {"query_type": query_classification_chain, "query": lambda x: x['query'], 'input': lambda x: x['query']}
+    {"query_type": query_classification_chain, 'input': lambda x: x['query']}
     | RunnableLambda(route)
     | parse_output
 )
