0531a600583865e0e098b2641854a7634bf0f041
Jay Shirgupe <jns29072003@gmail.com>
2024-10-12 22:10:52 +0530

    Removed wrong imports from chain_utils in qa.py which were causing errors in normal rag

--- server/chains/qa.py
+++ server/chains/qa.py
@@ -1,12 +1,12 @@
 from .query_classification import query_classification_chain
 from .table_query import csv_agent
-from .chain_utils import rag_chain
+from .rag_chain import rag_chain
 from langchain_core.runnables import RunnablePassthrough
 from langchain_core.runnables import RunnableLambda
 
 def route(info):
     if info['query_type'] == 'Data Retrieval':
-        return rag_chain.invoke(info['input'])
+        return rag_chain
     elif info['query_type'] == 'Data Computation':
         return csv_agent
 
